f331005179d388edcb02e16427a315ac80fe9bec
xlebyshek17 <kristina1111113@gmail.com>
2025-12-02 18:36:35 +0100

    initial commit

---
+++ build/CMakeFiles/lab06.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/kridtina/Рабочий стол/biblioteki_graficzne/stud_src/GUI.cpp" "CMakeFiles/lab06.dir/GUI.cpp.o" "gcc" "CMakeFiles/lab06.dir/GUI.cpp.o.d"
+  "/home/kridtina/Рабочий стол/biblioteki_graficzne/stud_src/GUIMyFrame1.cpp" "CMakeFiles/lab06.dir/GUIMyFrame1.cpp.o" "gcc" "CMakeFiles/lab06.dir/GUIMyFrame1.cpp.o.d"
+  "/home/kridtina/Рабочий стол/biblioteki_graficzne/stud_src/main.cpp" "CMakeFiles/lab06.dir/main.cpp.o" "gcc" "CMakeFiles/lab06.dir/main.cpp.o.d"
+  "/home/kridtina/Рабочий стол/biblioteki_graficzne/stud_src/vecmat.cpp" "CMakeFiles/lab06.dir/vecmat.cpp.o" "gcc" "CMakeFiles/lab06.dir/vecmat.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/lab06.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab06.dir/GUI.cpp.o"
+  "CMakeFiles/lab06.dir/GUI.cpp.o.d"
+  "CMakeFiles/lab06.dir/GUIMyFrame1.cpp.o"
+  "CMakeFiles/lab06.dir/GUIMyFrame1.cpp.o.d"
+  "CMakeFiles/lab06.dir/main.cpp.o"
+  "CMakeFiles/lab06.dir/main.cpp.o.d"
+  "CMakeFiles/lab06.dir/vecmat.cpp.o"
+  "CMakeFiles/lab06.dir/vecmat.cpp.o.d"
+  "lab06"
+  "lab06.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab06.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
